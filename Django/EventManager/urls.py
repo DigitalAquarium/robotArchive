7481ba5272d4f0cacc21895897d27703f8c5d575
Jonathan Caines <jec845@student.bham.ac.uk>
2021-12-20 17:28:55 +0000

    Basic template added

--- Django/EventManager/urls.py
+++ Django/EventManager/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("cheese/",include("main.urls")),
+    path("cheese/",include("main.urls"),name="main"),
     path("accounts/",include("django.contrib.auth.urls"))
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
