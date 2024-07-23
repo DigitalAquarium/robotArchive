ab8c3785fe44c7bddfb8ab2a512bb6eea19c690c
me57tm <jonathancaines1@gmail.com>
2024-07-24 00:30:07 +0100

    debug for battlebordle

--- Django/bbguessinggame/urls.py
+++ Django/bbguessinggame/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 
 from . import views
 
-app_name = 'bbGuessGame'
+# app_name = 'bbGuessGame'
 urlpatterns = [
     path('', views.indexView, name='index'),
     path('getByName', views.getByNameView, name='getByName'),
     path('getBotOfTheDay', views.getBotOfTheDayView, name='getBotOfTheDay'),
+    path("getDebugTimes",views.getDebugTimes,name="getDebugTimes"),
     path('match', views.matchView, name='match'),
 ]
